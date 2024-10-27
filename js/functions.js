1e05d8af20ba4a355b76cc98937a07507c4876d6
Vlad Koronchik <vladislavkoronchik@yandex.ru>
2024-10-27 13:03:08 +0300

    fix: correct linter errors

--- js/functions.js
+++ js/functions.js
@@ -19,21 +19,25 @@ const getNumbersFromString = (value) => {
   return +number;
 };
 
-console.log(isLessThanOrEqualToMaxLength('проверяемая строка', 20));
-console.log(isLessThanOrEqualToMaxLength('проверяемая строка', 18));
-console.log(isLessThanOrEqualToMaxLength('проверяемая строка', 10));
+isLessThanOrEqualToMaxLength('проверяемая строка', 18);
+isPalindrom('Лёша на полке клопа нашёл ');
+getNumbersFromString('1 кефир, 0.5 батона');
 
-console.log(isPalindrom('топот'));
-console.log(isPalindrom('ДовОд'));
-console.log(isPalindrom('Кекс'));
-console.log(isPalindrom('Лёша на полке клопа нашёл '));
-console.log(isPalindrom(777));
+// console.log(isLessThanOrEqualToMaxLength('проверяемая строка', 20));
+// console.log(isLessThanOrEqualToMaxLength('проверяемая строка', 18));
+// console.log(isLessThanOrEqualToMaxLength('проверяемая строка', 10));
 
-console.log(getNumbersFromString('2023 год'));
-console.log(getNumbersFromString('ECMAScript 2022'));
-console.log(getNumbersFromString('1 кефир, 0.5 батона'));
-console.log(getNumbersFromString('агент 007'));
-console.log(getNumbersFromString('а я томат'));
-console.log(getNumbersFromString(2023));
-console.log(getNumbersFromString(-1));
-console.log(getNumbersFromString(1.5));
+// console.log(isPalindrom('топот'));
+// console.log(isPalindrom('ДовОд'));
+// console.log(isPalindrom('Кекс'));
+// console.log(isPalindrom('Лёша на полке клопа нашёл '));
+// console.log(isPalindrom(777));
+
+// console.log(getNumbersFromString('2023 год'));
+// console.log(getNumbersFromString('ECMAScript 2022'));
+// console.log(getNumbersFromString('1 кефир, 0.5 батона'));
+// console.log(getNumbersFromString('агент 007'));
+// console.log(getNumbersFromString('а я томат'));
+// console.log(getNumbersFromString(2023));
+// console.log(getNumbersFromString(-1));
+// console.log(getNumbersFromString(1.5));
